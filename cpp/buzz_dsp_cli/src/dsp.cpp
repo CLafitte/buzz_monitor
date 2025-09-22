cb284f48f4e711bc1d0d0a58c3546712e97635ff
connorlafitteaudio <166548426+CLafitte@users.noreply.github.com>
2025-09-21 17:16:22 -0700

    Update dsp.cpp with Cooley-Tukey

--- cpp/buzz_dsp_cli/src/dsp.cpp
+++ cpp/buzz_dsp_cli/src/dsp.cpp
@@ -1,1 +1,24 @@
+#include "dsp.hpp"
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <complex>
+
+using cpx = std::complex<double>;
+
+// Recursive FFT
+void DSP::fft(std::vector<cpx>& x) {
+    size_t N = x.size();
+    if (N <= 1) return;
+
+    // Split into even and odd
+    std::vector<cpx> even(N/2), odd(N/2);
+    for (size_t i=0; i<N/2; ++i) {
+        even[i] = x[i*2];
+        odd[i] = x[i*2 + 1];
+    }
+
+    // Recursive calls
+    fft(even);
+    fft(odd);
 
