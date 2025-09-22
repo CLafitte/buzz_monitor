54b6f0f42d0178dcac88b95dee97387bf286f550
connorlafitteaudio <166548426+CLafitte@users.noreply.github.com>
2025-09-21 17:05:14 -0700

    Update main.cpp

--- cpp/buzz_dsp_cli/src/main.cpp
+++ cpp/buzz_dsp_cli/src/main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
 
+int main(int argc, char* argv[]) {
+    std::cout << "buzz_dsp_cli: Hello World!" << std::endl;
+
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " <input.wav> [options]" << std::endl;
+        return 1;
+    }
+
+    std::string input_file = argv[1];
+    std::cout << "Input file: " << input_file << std::endl;
+
+    // TODO: parse options, call DSP functions
+
+    return 0;
+}
